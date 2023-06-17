e30893cf7aa35c99f48a4b6c5b195b1d5cd22177
Shendy Krisyohanda <krisyohanda@gmail.com>
2023-06-17 21:42:05 +0800

    fix koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,7 @@
+
 <?php
 //Koneksi database prosedural mysqli
-$koneksi = mysqli_connect('localhost', 'root', '', 'perpustakaan');
+$koneksi = mysqli_connect('10.0.0.17', 'booklib', 'Berbagiituindah123!', 'perpustakaan', 3306);
 
 //Memeriksa koneksi
 if(!$koneksi){
