e9383e3796e091f764eeaa5f37c67deb400fa8bb
Zai <2010817310001@mhs.ulm.ac.id>
2023-06-18 02:56:23 +0800

    first

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-6 text-right">
-                Copyright 2023 <a href="#">BookLib</a> All Rights Reserved.
+                Copyright 2023 <a href="#">BookLib</a> All Rights Reserved. || Server 1
             </div>
         </div>
     </div>
