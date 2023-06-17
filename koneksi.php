1dca215123d90533f0905a97dcd36628f5fcfa43
Shendy Krisyohanda <krisyohanda@gmail.com>
2023-06-17 20:47:31 +0800

    Remove

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,6 @@
-
 <?php
 //Koneksi database prosedural mysqli
-$koneksi = mysqli_connect('10.0.0.17', 'booklib', 'Berbagiituindah123!', 'perpustakaan', 3306);
+$koneksi = mysqli_connect('localhost', 'root', '', 'perpustakaan');
 
 //Memeriksa koneksi
 if(!$koneksi){
