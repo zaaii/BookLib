d637e221ccf9d2b8a56a3b5a074efd6f7d53fedf
Zai <2010817310001@mhs.ulm.ac.id>
2023-06-19 14:41:23 +0800

    ffix buku

--- model.php
+++ model.php
@@ -35,7 +35,11 @@ function insertDataBuku($data)
     if (isset($_FILES["gambar_buku"]["tmp_name"]) && !empty($_FILES["gambar_buku"]["tmp_name"])) {
         $gambar_buku = $_FILES["gambar_buku"]["name"];
         $gambar_buku_tmp = $_FILES["gambar_buku"]["tmp_name"];
-        move_uploaded_file($gambar_buku_tmp, "resources/cover/" . $gambar_buku);
+        $gambar_buku_destination = "resources/cover/" . $gambar_buku;
+        $move_gbuku = "sudo mv " . escapeshellarg($gambar_buku_tmp) . " " . escapeshellarg($gambar_buku_destination);
+        $chmod_gbuku = "sudo chmod 0644 " . escapeshellarg($gambar_buku_destination);
+        exec($move_gbuku);
+        exec($chmod_gbuku);
     } else {
         $gambar_buku = '';
     }
@@ -44,7 +48,11 @@ function insertDataBuku($data)
     if (isset($_FILES["pdf_buku"]["tmp_name"]) && !empty($_FILES["pdf_buku"]["tmp_name"])) {
         $pdf_buku = $_FILES["pdf_buku"]["name"];
         $pdf_buku_tmp = $_FILES["pdf_buku"]["tmp_name"];
-        move_uploaded_file($pdf_buku_tmp, "resources/ebook/" . $pdf_buku);
+        $pdf_buku_destination =  "resources/ebook/" . $pdf_buku;
+        $move_pbuku = "sudo mv " . escapeshellarg($pdf_buku_tmp) . " " . escapeshellarg($pdf_buku_destination);
+        $chmod_pbuku = "sudo chmod 0644 " . escapeshellarg($pdf_buku_destination);
+        exec($move_pbuku);
+        exec($chmod_pbuku);
     } else {
         $pdf_buku = '';
     }
@@ -80,7 +88,11 @@ function updateDataBuku($data)
     if (isset($_FILES["gambar_buku"]["tmp_name"]) && !empty($_FILES["gambar_buku"]["tmp_name"])) {
         $gambar_buku = $_FILES["gambar_buku"]["name"];
         $gambar_buku_tmp = $_FILES["gambar_buku"]["tmp_name"];
-        move_uploaded_file($gambar_buku_tmp, "resources/cover/" . $gambar_buku);
+        $gambar_buku_destination = "resources/cover/" . $gambar_buku;
+        $move_gbuku = "sudo mv " . escapeshellarg($gambar_buku_tmp) . " " . escapeshellarg($gambar_buku_destination);
+        $chmod_gbuku = "sudo chmod 0644 " . escapeshellarg($gambar_buku_destination);
+        exec($move_gbuku);
+        exec($chmod_gbuku);
     } else {
         // Jika tidak ada perubahan, tetap gunakan gambar_buku sebelumnya
         $bukuSebelumnya = getData("buku WHERE id_buku = $id_buku")[0];
@@ -91,7 +103,11 @@ function updateDataBuku($data)
     if (isset($_FILES["pdf_buku"]["tmp_name"]) && !empty($_FILES["pdf_buku"]["tmp_name"])) {
         $pdf_buku = $_FILES["pdf_buku"]["name"];
         $pdf_buku_tmp = $_FILES["pdf_buku"]["tmp_name"];
-        move_uploaded_file($pdf_buku_tmp, "resources/ebook/" . $pdf_buku);
+        $pdf_buku_destination =  "resources/ebook/" . $pdf_buku;
+        $move_pbuku = "sudo mv " . escapeshellarg($pdf_buku_tmp) . " " . escapeshellarg($pdf_buku_destination);
+        $chmod_pbuku = "sudo chmod 0644 " . escapeshellarg($pdf_buku_destination);
+        exec($move_pbuku);
+        exec($chmod_pbuku);
     } else {
         // Jika tidak ada perubahan, tetap gunakan pdf_buku sebelumnya
         $bukuSebelumnya = getData("buku WHERE id_buku = $id_buku")[0];
